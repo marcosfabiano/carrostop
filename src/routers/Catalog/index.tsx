e39a524c6d8021d606a401bbe817d6539c0e51d9
Marcos Fabiano <marcos.fabiano67@outlook.com>
2022-10-28 12:13:02 -0300

    Search bar

--- src/routers/Catalog/index.tsx
+++ src/routers/Catalog/index.tsx
@@ -1,8 +1,22 @@
+import "./styles.css";
 import Header from "../../components/Header";
+import SearchBar from "../../components/SearchBar";
 
 export default function Catalog() {
+  return (
+    <>
+      <Header />
+      <main className="ct-catalog-main">
+        <section id="ct-catalog-section" className="ct-container">
+          <div className="ct-search-bar-container">
+            <SearchBar />
+          </div>
 
-    return (
-        <Header />
-    );
+          <div className="ct-catalog-cards-container">
+            <div>Teste</div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
