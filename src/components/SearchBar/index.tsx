aa0bc39873e742900a5309fe662fc7d1f5fff045
Marcos Fabiano <marcos.fabiano67@outlook.com>
2022-10-28 14:12:33 -0300

    Catalog cards

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,10 +1,16 @@
-import './styles.css';
+import "./styles.css";
 
 export default function SearchBar() {
   return (
     <form className="ct-search-bar">
-      <input className="ct-form-control" type="text" placeholder="Digite sua busca" />
-      <button className="ct-search-bar-submit" type="submit">Buscar</button>
+      <input
+        className="ct-form-control"
+        type="text"
+        placeholder="Digite sua busca"
+      />
+      <button className="ct-search-bar-submit" type="submit">
+        Buscar
+      </button>
     </form>
   );
 }
--- src/routers/Catalog/index.tsx
+++ src/routers/Catalog/index.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import Header from "../../components/Header";
 import SearchBar from "../../components/SearchBar";
+import Card from "../../components/Card";
 
 export default function Catalog() {
   return (
@@ -11,9 +12,17 @@ export default function Catalog() {
           <div className="ct-search-bar-container">
             <SearchBar />
           </div>
-
           <div className="ct-catalog-cards-container">
-            <div>Teste</div>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
           </div>
         </section>
       </main>
